a4aa7025c1df84b35d48a41d0af06345341d2709
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-24 11:15:30 +0200

    Create Solution.java

---
+++ Print Array in Reverse/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        int[] array = new int[T];
+        for(int i = 0; i < T; i++)
+        {
+            array[i] = scan.nextInt();
+        }
+        
+        for(int j = T - 1; j >= 0; j--)
+        {
+            System.out.print(array[j] + " ");
+        }
+        scan.close();
+    }
+}
