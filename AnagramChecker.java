f7d8cbc0a4b4ad529dfb95db8b56b8de10c02602
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-28 22:49:58 +0200

    Update AnagramChecker.java

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -1,3 +1,15 @@
+/**
+* Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+* An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+* Example 1:
+
+* Input: s = "racecar", t = "carrace"
+
+* Output: true
+**/
+
 class AnagramChecker 
 {
     public boolean isAnagram(String s, String t) 
