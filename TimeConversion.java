46742fc3a86d10c8c1fdb6623c173c34fe10e071
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-03 23:29:57 +0200

    Update TimeConversion.java

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,3 +1,26 @@
+/*Problem: Time Conversion
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note:
+12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Function Description
+Complete the function timeConversion in the editor below.
+
+The function timeConversion has the following parameter(s):
+
+string s: a time in 12-hour AM/PM format.
+Returns
+string: the time in 24-hour format.
+
+Input Format
+A single string s representing a time in 12-hour clock format (i.e., hh:mm:ssAM or hh:mm:ssPM).
+
+Constraints
+All input times are valid.
+*/
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -27,7 +50,7 @@ class Result
          
          if(s.endsWith("PM") && hour != 12)
          {
-             hour += 12; // convert to 24-hour
+             hour += 12; // convert to 24-hour time style
          }
          
          if(s.endsWith("AM") && hour == 12)
