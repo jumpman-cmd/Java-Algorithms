d1ba930736fc4d829aed8d3275f923174dae8b9f
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-04 15:46:36 +0200

    Update CountingSort1.java

--- CountingSort1.java
+++ CountingSort1.java
@@ -1,3 +1,22 @@
+/*Problem Statement: Counting Sort Frequency Array
+Objective:
+Complete the function countingSort in the editor below, which takes a parameter: a list of integers. The function must return a frequency array that indicates how many times each integer appears in the list.
+
+Function Description:
+countingSort has the following parameter(s):
+List<Integer> arr: a list of integers where each integer is in the range 0 <= arr[i] < 100.
+
+Returns:
+List<Integer>: A list of 100 integers, where each index i contains the frequency of the integer i in the input list.
+
+Input Format:
+The first line contains an integer n, the number of items in arr.
+Each of the next n lines contains an integer arr[i] where 0 <= arr[i] < 100
+
+Output Format:
+Return a list of 100 integers representing the frequency of each integer in the range [0, 99].
+*/
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
