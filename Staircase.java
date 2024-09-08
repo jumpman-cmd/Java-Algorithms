426cbce20c05fdaa597449f0c9656276b00aecf5
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-08 12:26:48 +0200

    Update Staircase.java

--- Staircase.java
+++ Staircase.java
@@ -1,3 +1,12 @@
+/*
+Problem Statement: Staircase
+Write a program to print a staircase of size n using the # symbol. The staircase will consist of n levels, and each level should be right-aligned. 
+The staircase is constructed such that each level contains a number of spaces followed by # symbols.
+
+e.g., For an input of n = 6, the staircase has 6 levels. The first level has 5 spaces and 1 #, the second level has 4 spaces and 2 #, and so on, 
+until the last level, which has no spaces and 6 # symbols. Each level is printed on a new line
+*/
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
