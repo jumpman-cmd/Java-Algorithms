9242133ade1045ecea0963eb05af7269f2050073
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-14 22:26:31 +0200

    Create GridChallenge.java

---
+++ GridChallenge.java
@@ -0,0 +1,95 @@
+// I am still working on it, it gives me errors when I run it.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    /*
+     * Complete the 'gridChallenge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+     
+     public static String gridChallenge(List<String> grid) 
+     {
+         // Write your code here
+         int num = grid.size();
+         
+         for(int i = 0; i < num; i++)
+         {
+             char[] gridArrays = grid.get(i).toCharArray();
+             Arrays.sort(gridArrays);
+             grid.set(i, new String(gridArrays));
+         }
+         
+         for(int j = 0; j < num; j++) // j = columns
+         {
+             for(int k = 1; k < num; k++) // k = rows
+             {
+                 if(grid.get(k).charAt(j) < grid.get(k - 1).charAt(j))
+                 {
+                     return "NO";
+                 }
+             }
+        }
+        
+        return "YES";
+    }
+}
+
+public class GridChallenge 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> 
+        {
+            try 
+            {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<String> grid = IntStream.range(0, n).mapToObj(i -> 
+                {
+                    try 
+                    {
+                        return bufferedReader.readLine();
+                    } 
+                    
+                    catch (IOException ex) 
+                    {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String result = Result.gridChallenge(grid);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } 
+            
+            catch (IOException ex) 
+            {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
