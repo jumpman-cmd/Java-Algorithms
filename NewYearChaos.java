74823f1c868e87edd66d7ec9ce86833c16a871c6
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-15 19:03:02 +0200

    Update NewYearChaos.java

--- NewYearChaos.java
+++ NewYearChaos.java
@@ -1,3 +1,20 @@
+/*
+Problem Statement: New Year Chaos
+On New Year's Day, people are lined up for a rollercoaster ride, each wearing a sticker indicating their original position in the queue, numbered from 1 to n. 
+Each person can bribe the person directly in front of them to swap positions, but they can bribe at most two others.
+
+The task is to determine the minimum number of bribes that took place to achieve a given final queue order. If anyone has bribed more than two people, print "Too chaotic".
+Input Format:
+    The first line contains an integer t, the number of test cases.
+    
+    For each test case:
+        An integer n, the number of people in the queue.
+        A list of n space-separated integers representing the final state of the queue.
+
+Output Format:
+    For each test case, print the minimum number of bribes or "Too chaotic" if the condition is violated.
+*/
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
