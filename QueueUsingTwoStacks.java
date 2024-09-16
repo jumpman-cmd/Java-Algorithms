eb414a13c8343072f90ed7f9062a3e1ae00dbf7b
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-16 19:25:53 +0200

    Update QueueUsingTwoStacks.java

--- QueueUsingTwoStacks.java
+++ QueueUsingTwoStacks.java
@@ -1,5 +1,23 @@
 /*
+Problem Statement: Implement a Queue Using Two Stacks
+The task is to implement a queue using two stacks. A queue follows a First-In-First-Out (FIFO) structure, 
+where elements are enqueued at the rear and dequeued from the front. The program will process a series of queries that either enqueue an element, 
+dequeue an element, or print the element at the front of the queue.
 
+Implement must follow three types of queries:
+    1 x: Enqueue element x into the end of the queue.
+    2: Dequeue the element from the front of the queue.
+    3: Print the element at the front of the queue.
+
+Input:
+    The first line contains an integer q (the number of queries).
+    Each of the following q lines contains a query of one of the three types mentioned above.
+
+Output:
+    For each query of type 3, print the element at the front of the queue.
+
+Constraints:
+    It is guaranteed that all dequeue and print operations will be valid, i.e., the queue will never be empty when such operations are performed.
 */
 
 import java.io.*;
