4148be792605214d2187b27495dcde4c4047edec
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-17 14:00:21 +0200

    Update QueueUsingTwoStacks.java

--- QueueUsingTwoStacks.java
+++ QueueUsingTwoStacks.java
@@ -23,54 +23,56 @@ Constraints:
 import java.io.*;
 import java.util.*;
 
-public class QueueUsingTwoStacks 
+public class Solution 
 {
     public static void main(String[] args) 
     {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner count = new Scanner(System.in);
-        int numberOfOperations = count.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int numberOfOperations = scan.nextInt();
         
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        Stack<Integer> quStack1 = new Stack<>();
+        Stack<Integer> quStack2 = new Stack<>();
         
         for(int i = 0; i < numberOfOperations; i++)
         {
-            int typeOfOperation = count.nextInt();
-            
+            int typeOfOperation = scan.nextInt();
             if(typeOfOperation == 1)
             {
-                int addToQu = count.nextInt();
-                stack1.push(addToQu);
+                int x = scan.nextInt();
+                quStack1.push(x);
             }
             
             else if(typeOfOperation == 2)
             {
-                if(stack2.isEmpty())
+                if(quStack2.isEmpty())
                 {
-                    while(!stack1.isEmpty())
+                    while(!quStack1.isEmpty())
                     {
-                        stack2.push(stack1.pop());
+                        quStack2.push(quStack1.pop());
                     }
                 }
                 
-                stack2.pop();
+                quStack2.pop();
             }
             
             else
             {
-                if(typeOfOperation == 3 && stack2.isEmpty())
+                if(typeOfOperation == 3)
                 {
-                    while(!stack1.isEmpty())
+                    if(quStack2.isEmpty())
                     {
-                        stack2.push(stack1.pop());
+                        while(!quStack1.isEmpty())
+                        {
+                            quStack2.push(quStack1.pop());
+                        }
                     }
                 }
                 
-                System.out.println(stack2.peek());
+                System.out.println(quStack2.peek());
             }
         }
         
-        count.close();
+        scan.close();
     }
 }
