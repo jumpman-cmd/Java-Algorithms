29d51e2e9fb6e643b1201274827176f025c27ed2
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-28 23:54:09 +0200

    Update NoPrefixSet.java

--- NoPrefixSet.java
+++ NoPrefixSet.java
@@ -1,3 +1,18 @@
+/*
+No Prefix Set
+
+You are given a list of strings consisting only of lowercase letters. A set of strings is considered a GOOD SET if no string in the set is a prefix of another string. 
+If any string is a prefix of another, print BAD SET followed by the first offending string.
+
+Input
+    The first line contains an integer n (1 ≤ n ≤ 100,000), the number of strings.
+    The next n lines each contain a string words[i] (1 ≤ |words[i]| ≤ 100), composed of lowercase letters.
+
+Output
+    Print GOOD SET if the strings form a good set.
+    If there is a bad set, print BAD SET followed by the offending string on the next line.
+*/
+
 import java.io.*;
 import java.util.*;
 
