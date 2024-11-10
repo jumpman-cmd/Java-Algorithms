cebd193a21ccdd7d6cf79db4d9359352a0ee88f6
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-11-11 00:55:42 +0200

    Update Duplicate Integer.java

--- P/Duplicate Integer.java
+++ P/Duplicate Integer.java
@@ -20,3 +20,28 @@ class Solution
         return no;
     }
 }
+
+// or
+
+class Solution 
+{
+    public int findDuplicate(int[] nums)
+    {
+        int n = nums.length;
+        int duplicate = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    duplicate = nums[i];
+                    return duplicate;
+                }
+            }
+        }
+
+        return -1;      
+    }
+}
